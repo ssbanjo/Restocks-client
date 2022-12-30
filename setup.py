7225e224f898d0b7ce541baab21ab740a32f6e94
SSbanjo <oscargd02@gmail.com>
2022-12-30 12:34:33 +0100

    added `__version__.py`

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from asyncore import read
 from setuptools import setup, find_packages
+import os
 
-VERSION = '0.1.1'
-DESCRIPTION = 'Restocks.net client for Python'
-
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "restocks", "__version__.py"), "r", encoding="utf-8") as f:
+    exec(f.read(), about)
+    
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
-
 setup(
-    name="restocks-client",
-    version=VERSION,
-    author="banjo",
-    description=DESCRIPTION,
+    name=about["__title__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    description=about["__description__"],
+    license=about["__license"],
     packages=find_packages(),
     install_requires=["requests", "beautifulsoup4", "lxml"],
-    license="MIT",
     long_description=readme,
     long_description_content_type="text/markdown",
     keywords=["python", "client"],
